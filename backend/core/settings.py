2faa0b22ff27df901d36cad17ce3e1658492b162
Matheus C. Santos <eumcsantos@gmail.com>
2026-02-03 18:32:31 -0400

    Funcionalidade: criar aplicativo de análises (analyses) e registrar nas configurações

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Apps padrão do Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps de terceiros
+    'rest_framework',
+    'corsheaders',
+    # Seus Apps
+    'analyses',
 ]
 
 MIDDLEWARE = [
